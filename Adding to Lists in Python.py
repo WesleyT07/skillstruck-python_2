e2566287b5bf490ec3bac6a251c831d06c7831ce
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:03 -0700

    exported from Skill Struck

---
+++ Adding to Lists in Python.py
@@ -0,0 +1,22 @@
+#This is the checkpoint
+#flowers = ["rose", "tulip", "lilac", "sunflower"]
+#flowers.append("Dog")
+#flowers.extend(['Cat', 'Mouse'])
+#flowers.insert(4, "Rat")
+#print(flowers)
+#Last Minute Treats challenge
+#treats = ["popcorn", "popsicles", "soda", "chips", "cookies"]
+#treat1 = input("What is the first treat you would like to add?")
+#treat2 = input("What is the first treat you would like to add")
+#treats.append(treat1)
+#treats.append(treat2)
+#print(treats)
+#Donation challenge
+#money = [int(n) for n in input("How much was each purchase? (seperate each purchase with spaces)").split()]
+#cash_back = []
+
+#for x in money:
+#	if x >= 5:
+#		x = round(x * 0.1, 1)
+#		cash_back.append(x)
+#print(cash_back)
