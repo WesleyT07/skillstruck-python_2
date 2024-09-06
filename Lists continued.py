cf606b2072549282550d786560c3b304d3685d81
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:04 -0700

    exported from Skill Struck

---
+++ Lists continued.py
@@ -0,0 +1,15 @@
+#checkpoint
+#list1 = ['dog', 'cat', 'mouse', 'chicken', 'tree', 'house', 'grass']
+#print(list1[0:3])
+#list1[6] = "pool"
+#print(list1)
+#print(len(list1))
+#Find the Largest Value challenge
+#my_list = [int(n) for n in input("Enter numbers:").split()]
+#my_list.sort(reverse=True)
+#print(my_list[0])
+#Swap Neighbors challenge
+#inp = [int(n) for n in input("Enter numbers:").split()]
+#for i in range(0, len(inp) - 1, 2):
+#	inp[i], inp[i + 1] = inp[1 + i], inp[i]
+#print(inp)
