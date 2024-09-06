fb0616f8bc311132a7d9da94fa940d6009e57eb4
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:02 -0700

    exported from Skill Struck

---
+++ Accessing Items in a List.py
@@ -0,0 +1,11 @@
+#Checkpoint didn't save.
+#First Two, Last Two challenge
+#numbers = [int(n) for n in input("Enter 6 numbers").split()]
+#print(numbers[0] + numbers[1] + numbers[4] + numbers[5])
+#Boat Cruise challenge
+#string = "On our boat cruise we saw crocodiles, flamingos, turtles, fish, and even a manatee!"
+#letter = input("input the letter")
+#if letter in string:
+#    print(string.split(letter))
+#else:
+#    print("The letter " + letter + " is not in the string")
