b65667b3d81d66599ee84e66d73fd8d8dba0e32f
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:03 -0700

    exported from Skill Struck

---
+++ removing from lists.py
@@ -0,0 +1,19 @@
+#this is the checkpoint
+#harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+#harvest.remove("squash")
+#app = harvest.pop(1)
+#print(harvest)
+#Skills challenge
+#skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+#skil = input("what is your skill?")
+#skills.remove(skil)
+#print(skills)
+#Remove the Center Number challenge
+#inpu = input("Enter your words:").split()
+#center = int(len(inpu)/2)
+#if len(inpu) %2 == 1:
+#	inpu.pop(center)
+#else:
+#	inpu.pop(center)
+#	inpu.pop(center - 1)
+#print(inpu)
