91cfbf6bc7bc12941db557358ab63aab6507f469
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:01 -0700

    exported from Skill Struck

---
+++ If statement in for loop.py
@@ -0,0 +1,23 @@
+#This was the checkpoint.
+#gross = ["sour cream", "Asparagus", "mayo", "Boiled or deviled egg"]
+#for x in gross:
+#    if x == "mushrooms":
+#        print(x + " are gross")
+#    elif x == "broccoli":
+#        print(x + " is gross")
+#    elif x == "fish":
+#        print(x + " is gross")
+#    else:
+#        print(x + ' is gross')
+#this is Count the Zeros challenge
+#numbers = [int(n) for n in input("Input a list of numbers and include zeros").split()]
+#total = 0
+#for x in numbers:
+#    if x == 0:
+#        total += 1
+#print(total)
+#Find the even numbers challenge
+#numbers = [int(n) for n in input("Input a list of numbers").split()]
+#for x in numbers:
+#    if x%2 is 0:
+#        print(x)
