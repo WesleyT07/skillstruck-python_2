00a86e558b2fac9e647bb9ad1868092b39d64461
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-06 14:16:02 -0700

    exported from Skill Struck

---
+++ using integers from a list.py
@@ -0,0 +1,25 @@
+#This is the checkpoint
+#lasers = [3, 10, 4, 15, 11]
+#print(lasers[2]*10)
+#print(round(3.14159265,3))
+#for x in range(0,10):
+#    print(x)
+#for x in range(0,10,2):
+#    print(x)
+#Add the List challenge
+#numbers = [int(n) for n in input("Insert numbers").split()]
+#total = 0
+#for x in numbers:
+#    total += x
+#print(total)
+#Get Info Fast challenge
+#my_list = ["James", 10, "blue", "Zoe", 8, "red", "Dan", 12, "pink", "Shana", 11, "orange", "Sebastian", 9, "yellow", "Cynthia", 13, "green"]
+#inp = input("Do you want to know name, age, or favorite color?")
+#if inp == "name":
+#    print(my_list[0:18:3])
+#elif inp == "age":
+#    print(my_list[1::3])
+#elif inp == "favorite color":
+#    print(my_list[2::3])
+#else:
+#    print("Invalid Input")
