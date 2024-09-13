e94646cbbac313be174e1a3f88c54c37b01087ca
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-13 13:08:00 -0700

    exported from Skill Struck

---
+++ multiple parameters.py
@@ -0,0 +1,24 @@
+#Checkpoint
+#def functionn(first,second):
+#    print("I like to eat " + first + second)
+#functionn("cheese", "burger")
+#functionn("water", "melon")
+#Hungry for Apples 3 challenge
+#fruit1 = input("What's the first fruit?")
+#fruit2 = input("What's the second fruit?")
+#fruit3 = input("What is the last fruit?")
+#def eat(one, two, three):
+#    print(one + two + three)
+#eat(fruit1, fruit2, fruit3)
+#Spot the difference challenge
+#number1 = int(input("enter first number"))
+#number2 = int(input("Enter ssecond number"))
+#number3 = int(input("enter third number"))
+#def num(first, second, third):
+#    if first == second:
+#        print("3")
+#    elif second == third:
+#        print("1")
+#    else:
+#        print("2")
+#num(number1, number2, number3)
