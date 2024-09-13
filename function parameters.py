374d534509a4ff3ea19cf4440c56844bf8c57d97
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-13 13:07:58 -0700

    exported from Skill Struck

---
+++ function parameters.py
@@ -0,0 +1,20 @@
+#checkpoint
+#def function(yes):
+#    print("string " + yes)
+#function("here")
+#function("there")
+#function("everywhere")
+#hungry for apples 2 challenge
+#food = input("What food would you like?")
+#def the_food(choice):
+#    print(choice * 3)
+#the_food(food)
+#month = int(input("pick a month 1-12:"))
+#def days(number):
+#    if month == 1 or month == 3 or month == 5 or #month == 7 or month == 10 or month == 12:
+#        print("31")
+#    elif month == 4 or month == 6 or month == 9 or #month == 11:
+#        print("30")
+#    else:
+#        print("28")
+#days(month)
