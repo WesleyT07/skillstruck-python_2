6c86eb7f14d537cf877617949f3298e5978c06ed
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-13 13:07:49 -0700

    exported from Skill Struck

---
+++ while loops.py
@@ -0,0 +1,20 @@
+#checkpoint
+#number = 1
+#while number < 50:
+#    print(number)
+#    number += 1
+#How Many Numbers challenge
+#total = 0
+#number = int(input("Guess the number 1-10"))
+#while number > 0:
+#    number = int(input("Guess the number #1-10"))
+#    total += 1
+#print(total)
+#Largest number challenge
+#my_number = int(input("Pick a number:"))
+#largest = my_number
+#while my_number != 0:
+#	my_number = int(input("Pick a number:"))
+#	if my_number > largest:
+#		largest = my_number 
+#print(largest)
