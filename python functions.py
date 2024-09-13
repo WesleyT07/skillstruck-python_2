82b1cf1b5fc54622c0db8d6496ebef80ae103a2b
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-13 13:07:53 -0700

    exported from Skill Struck

---
+++ python functions.py
@@ -0,0 +1,23 @@
+#Checkpoint
+#def race():
+#    print("You won the race!")
+#race()
+#Start Button challenge
+#init = input("do you want to start?").lower()
+#def start():
+#    if init == "yes":
+#        print("Initialization Complete")
+#    else:
+#        print("Initialization Failed")
+#start()
+#Cube calculations challenge
+#length = 10
+#def surface_area():
+#    print(length * length * 6)
+
+#surface_area()
+
+#def volume():
+#    print(length * length * length)
+
+#volume()
