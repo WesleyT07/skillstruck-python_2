85cfe69d865f9c1355bfa612a0d3e40f95223c49
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-21 08:52:46 -0700

    exported from Skill Struck

---
+++ python dictionaries.py
@@ -0,0 +1,34 @@
+#This is the checkpoint
+#mountains = {
+#    "Timpanogos": 3,
+#    'Everest': 1,
+#    'Kilimanjaro': 2,
+#    'Vesuvius': 4
+#}
+#print(mountains)
+#print(mountains['Vesuvius'])
+#mountains["Kilimanjaro"] = 15
+#Dictionaries and Lists challenge
+#dic = {
+#    'first': 1,
+#    'sec': 2,
+#    'third': 3,
+#    'fou': 4,
+#    'five': 5
+#}
+#lis = ['first', 'second', "third", 'fourth', 'fifth']
+#print(dic)
+#print(lis)
+#Colors challenge
+#colors = {
+#    'red': 1,
+#    "orange": 2,
+#    'blue': 3,
+#    'purple': 4,
+#    'yes': 5
+#}
+#print(colors['orange'])
+#print(colors['purple'])
+#colors['red'] = 15
+#colors['blue'] = 20
+#print(colors)
