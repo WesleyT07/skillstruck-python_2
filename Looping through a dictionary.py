63e32e621ece65c6fe924d2fddc1d9bee815bde7
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-21 08:52:48 -0700

    exported from Skill Struck

---
+++ Looping through a dictionary.py
@@ -0,0 +1,40 @@
+#This is the checkpoint
+#measurement = {
+#    "Length": 24,
+#    "Width": 32,
+#    "Depth" : 10
+#}
+#for x in measurement.values():
+#    print(x)
+#Secret beach challenge
+#group = { 
+#    "Fred" : 12, 
+#    "Jackson" : 15, 
+#    "Sophie" : 20, 
+#    "Amanda" : "?", 
+#    "Jane" : "?", 
+#}
+
+#ama = int(input("Amanda:"))
+#group["Amanda"] = ama
+#Ja = int(input("Jane:"))
+#group["Jane"] = Ja
+#total = 0
+#for x in group.values():
+#    total += x
+#print(total)
+#add it all together challenge
+#group = { 
+#    3 : 10, 
+#    5 : 3, 
+#    10 : 6, 
+#    4 : 30, 
+#}
+
+#key = int(input("Key:"))
+#val = int(input("Value"))
+#group[key] = val
+#total = 0
+#for x,y in group.items():
+#    total += x * y
+#print(total)
