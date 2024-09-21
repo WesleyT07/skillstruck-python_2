9613beb0a1e27adada0b9101af27ca31e02a4cd8
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-21 08:52:47 -0700

    exported from Skill Struck

---
+++ dictionaries continued.py
@@ -0,0 +1,37 @@
+#checkpoint
+#coins = {
+#    "pennies": 20,
+#    "nickels": 2,
+#    "dimes": 3,
+#    "quarters": 13
+#}
+#coins["silver dollar"] = 3
+#coins.pop("pennies")
+#print(coins)
+#print(len(coins))
+#how many challenge
+#dictionary = { 
+#    1: "bicycle",
+#    2: "soccer balls", 
+#    3: "piano books",
+#}
+#four = input("What is something you have 4 of?")
+#five = input("What is something you have 5 of?")
+#six = input("What is something you have 6 of?")
+#dictionary[4] = four
+#dictionary[5] = five
+#dictionary[6] = six
+#print(dictionary)
+#Work schedule challenge
+work = {
+    "Monday": 8,
+    "Tuesday": 8,
+    "Wednesday": 8,
+    "Thursday": 8,
+    "Friday": 8
+}
+work["Saturday"] = 8
+work.pop("Wednesday")
+print(len(work))
+if "Friday" in work:
+    print('Friday is in dictionary.')
