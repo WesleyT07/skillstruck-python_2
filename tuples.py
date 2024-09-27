53409be6070d79317f4719c5611048a8bf937a62
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-09-27 12:11:08 -0700

    exported from Skill Struck

---
+++ tuples.py
@@ -0,0 +1,19 @@
+#Tuples are unchangeable
+#Checkpoint
+#grades = ("95", "70", "85", "92", "100")
+#print(grades)
+#print(grades[-2])
+#print(grades[0:3])
+#Check for a number challenge
+#friends = (3, 5, 7, 8, 10, 2, 12, 4)
+
+#number = int(input("What number?"))
+#if number in friends:
+#    print("Yes")
+#else:
+#    print("No")
+#Third from last challenge
+tupL = ("Dog", "Cat", "Mouse", "House", "Tree", "Grass", "Yes", "No")
+print(tupL[-3])
+print(tupL[5])
+print(tupL[0:4])
